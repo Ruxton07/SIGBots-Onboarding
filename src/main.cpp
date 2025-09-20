a14a352a644adc73b2d21a5bb2b202450f07292e
Ryan Kellar <rtkellar@Ryans-MacBook-Pro-2.local>
2025-09-20 11:04:27 -0400

    Added autos in teleop

--- src/main.cpp
+++ src/main.cpp
@@ -77,11 +77,16 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::lcd::set_text(1, "teleop started");
+	pros::lcd::set_text(4, "teleop started");
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-	pros::MotorGroup flywheel_mg({7}); // Creates a motor group with forwards ports 7
+	pros::MotorGroup left_mg({19});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({-18});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::MotorGroup flywheel_intake_mg({-7});
+	pros::MotorGroup flywheel_outtake_mg({9});
+	pros::Motor flywheel_out (9);
+	// Set up ADI Line follower with 3 wire ports G and H
+	pros::adi::Port line_follower_left('H');
+	pros::adi::Port line_follower_right('G');
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
@@ -90,13 +95,103 @@ void opcontrol() {
 
 		// Arcade control scheme
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
+		int turn = master.get_analog(ANALOG_LEFT_X);  // Gets the turn left/right from right joystick
 
 		pros::lcd::print(1, "dir: %d", dir);     // Prints the forward/backward value to the LCD
 		pros::lcd::print(2, "turn: %d", turn);   // Prints the turn value to the LCD
 
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);
-		flywheel_mg.move(master.get_analog(ANALOG_RIGHT_Y));  // Sets flywheel motor voltage
+		int last_line_value = 0; // value to store the last line sensor value: 0 = left, 1 = right
+		int WHITE_THRESHOLD = 2000; // threshold val to see if the sensor is on white or black
+
+		// flywheel_out.move_voltage(127); // always run the outtake flywheel at full speed
+		// left_mg.move(dir - turn);    // Sets left motor voltage
+		// right_mg.move(dir + turn);
+		flywheel_intake_mg.move(master.get_analog(ANALOG_RIGHT_Y));  // intake on the amt the down stick
+		flywheel_outtake_mg.move(master.get_analog(ANALOG_LEFT_X));   // run outtake based on how negative the right stick y is
+		pros::lcd::print(3, "running either intake? : %d", master.get_digital(DIGITAL_DOWN) || master.get_digital(DIGITAL_UP));
+		/* // Autonomous Teleop:
+		// Sensors have a gap barely the width of the line, so the robot will be off track when neither line sensor detects the line
+		// Use the left and right line sensors to follow the line
+		// If only one line follower detects, log which one
+		// Give the robot 30 seconds to get to the intake place
+		pros::lcd::print(5, "LF Left: %d", line_follower_left.get_value());
+		pros::lcd::print(6, "LF Right: %d", line_follower_right.get_value());
+		// the above should be between 0 and 4095
+
+		while (pros::millis() < 30000) {
+			// follow the line for 30 seconds
+			if (line_follower_left.get_value() < WHITE_THRESHOLD && line_follower_right.get_value() < WHITE_THRESHOLD) {
+				// both online normal forward
+				left_mg.move(100);
+				right_mg.move(100);
+			} else if (line_follower_left.get_value() < WHITE_THRESHOLD) {
+				// only left on line then slight turn left		
+				left_mg.move(50);
+				right_mg.move(100);
+				last_line_value = 0;
+			} else if (line_follower_right.get_value() < WHITE_THRESHOLD) {
+				// only right on line then slight turn right
+				left_mg.move(100);
+				right_mg.move(50);
+				last_line_value = 1;
+			} else {
+				// both off line then turn left/right (depending on which sensor was last on the line) until at least one sensor on line
+				if (last_line_value == 0) {
+					// last on left so turn left
+					left_mg.move(-50);
+					right_mg.move(50);
+				} else {
+					// last on right so turn right
+					left_mg.move(50);
+					right_mg.move(-50);
+				}
+			}
+		}
+
+		// now try to send balls in box to goal:
+		// drive forward while spinning up flywheel_intake (for 5 seconds)
+		// then drive backwards for 10 seconds with both flywheel motor groups off
+		// pray its in a good position and just run flywheel_outtake_mg for 5 seconds and hope
+		left_mg.move(100);
+		right_mg.move(100);
+		flywheel_intake_mg.move(127);
+		pros::delay(5000);
+		left_mg.move(-100);
+		right_mg.move(-100);
+		flywheel_intake_mg.move(0);
+		flywheel_outtake_mg.move(0);
+		pros::delay(10000);
+		left_mg.move(0);
+		right_mg.move(0);
+		flywheel_outtake_mg.move(127);
+		pros::delay(5000); */
+
+		// Super scuffed auton:
+		// Drive forward for 10 seconds with flywheel_intake_mg on max for the last 5 seconds
+		// then drive backwards for 10 seconds with both flywheel motor groups off
+		// pray its in a good position and just run flywheel_outtake_mg for 5 seconds
+		
+		// wait for digital left button to be pressed to start the auton
+		
+		if (master.get_digital(DIGITAL_LEFT)) {
+			while (true) {
+				pros::delay(20);
+				left_mg.move(80);
+				right_mg.move(127);
+				pros::delay(5000);
+				flywheel_intake_mg.move(127);
+				pros::delay(5000);
+				left_mg.move(-80);
+				right_mg.move(-127);
+				flywheel_intake_mg.move(0);
+				flywheel_outtake_mg.move(0);
+				pros::delay(10000);
+				left_mg.move(0);
+				right_mg.move(0);
+				flywheel_intake_mg.move(127);
+				flywheel_outtake_mg.move(127);
+				pros::delay(5000);
+			}
+		}
 	}
 }
